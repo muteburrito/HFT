c17f8970a2654a9c89c43b4ddbfc6b1a1537c73d
muteburrito <kulkarnichinmay65@gmail.com>
2025-12-24 08:55:43 +0530

    feat: Refactor main.py for bundled application support and add CI/CD workflow for build and release

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import sys
+import os
 from streamlit.web import cli as stcli
 
 def main():
-    sys.argv = ["streamlit", "run", "app.py"]
+    if getattr(sys, 'frozen', False):
+        # If the application is run as a bundle, the PyInstaller bootloader
+        # extends the sys module by a flag frozen=True and sets the app 
+        # path into variable _MEIPASS'.
+        application_path = sys._MEIPASS
+    else:
+        application_path = os.path.dirname(os.path.abspath(__file__))
+
+    app_path = os.path.join(application_path, "app.py")
+
+    sys.argv = ["streamlit", "run", app_path, "--global.developmentMode=false"]
     sys.exit(stcli.main())
 
 if __name__ == "__main__":
